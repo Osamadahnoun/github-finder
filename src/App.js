7861ed24c9c7d53eede8711bbd157864f4f52714
Osama Dahnoun <omdahnou@ncsu.edu>
2022-05-09 20:17:38 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar title="Github finder" />
+
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route element={<About />} exact path="/about" />
+            <Route element={<Home />} exact path="/" />
+            <Route element={<NotFound />} exact path="/notfound" />
+            <Route element={<NotFound />} exact path="/*" />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
